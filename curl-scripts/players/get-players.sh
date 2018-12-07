c2ba2581a6fd803152811bdc969776bd4943d974
jooewoo <jooewoo@gmail.com>
2018-12-07 09:22:52 -0500

    Added get all players curl script

--- curl-scripts/players/get-players.sh
+++ curl-scripts/players/get-players.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 # BAhJIiUwZmQ3YzRiMTAyODYwMTBkMzM2ZGQ0ODI3MDhkZDNkNgY6BkVG--684e137089e2655e1e96d944422a506f4ae3f965
-curl "http://localhost:4741/players/${ID}" \
+curl "http://localhost:4741/players/" \
   --include \
   --request GET \
   --header "Authorization: Token token=${TOKEN}"
-  --data '{
-    "player": {
-      "user_id": "'"${USER}"'"
-    }
-  }'
+
 
 echo
